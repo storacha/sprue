c5b1a5d270d4538706f4688f05ffd87e580c04d3
Vicente Olmedo <vicente@storacha.network>
2026-03-19 13:08:27 +0100

    init storoku

---
+++ deploy/shared/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 5.86.0"
+    }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+  }
+  backend "s3" {
+    bucket = "storacha-terraform-state"
+    key = "storacha/${var.app}/shared.tfstate"
+    region = "us-west-2"
+    encrypt = true
+  }
+}
+
+provider "aws" {
+  allowed_account_ids = [var.allowed_account_id]
+  region = "us-west-2"
+  default_tags {
+    tags = {
+      Environment   = "shared"
+      ManagedBy     = "OpenTofu"
+      Owner         = "storacha"
+      Team          = "Storacha Engineering"
+      Organization  = "Storacha"
+      Project       = "${var.app}"
+    }
+  }
+}
+
+provider "aws" {
+  alias = "dev"
+  allowed_account_ids = [var.allowed_account_id]
+  region = "us-east-2"
+  default_tags {
+    tags = {
+      Environment   = "dev"
+      ManagedBy     = "OpenTofu"
+      Owner         = "storacha"
+      Team          = "Storacha Engineering"
+      Organization  = "Storacha"
+      Project       = "${var.app}"
+    }
+  }
+}
+
+module "shared" {
+  source = "github.com/storacha/storoku//shared?ref=v0.6.2"
+  providers = {
+    aws = aws
+    aws.dev = aws.dev
+  }
+  create_db = false
+  caches = []
+  networks = []
+  app = var.app
+  create_shared_dev_resources = var.create_shared_dev_resources
+  zone_id = ""
+  domain_base = var.domain_base
+  setup_cloudflare = false
+}
